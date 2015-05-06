0dadd53553388a51df952dc98ff3fa741f0e7a36
mhuziv <mhusiv@ksf.kiev.ua>
2015-05-06 20:19:59 +0300

    Add utils

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("test");
         // TODO code application logic here
     }
     
