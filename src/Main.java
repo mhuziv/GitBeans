59ec4333a79ba2deff7f2e84d8f111b201f78334
mhuziv <mhusiv@ksf.kiev.ua>
2015-05-07 13:28:58 +0300

    Add utils

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test3");
         // TODO code application logic here
     }
     
